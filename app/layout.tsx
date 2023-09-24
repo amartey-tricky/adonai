958a67b9148e0328bd89690ec58e7e5552389d73
Patrick Amartey Annang <102452248+amartey-tricky@users.noreply.github.com>
2023-09-24 13:04:24 +0000

    start dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "./providers";
 import Nav from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Nav />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/Hero-h1.tsx
@@ -0,0 +1,5 @@
+import Typed from "typed.js";
+
+export default function HeroH1() {
+  return <h1>Hello</h1>;
+}
