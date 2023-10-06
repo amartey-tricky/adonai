9b80d7ef0e0d557dc00d0abdfe50d6be79020d8c
tricky <trickya23@gmail.com>
2023-10-06 11:20:30 +0000

    typed hero section

--- components/Hero-h1.tsx
+++ components/Hero-h1.tsx
@@ -1,5 +1,23 @@
+import { useRef, useEffect } from "react";
 import Typed from "typed.js";
 
 export default function HeroH1() {
-  return <h1>Hello</h1>;
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["<i>Welcome> to Adonai", "&amp; The church of your destiny"],
+      typeSpeed: 40,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <h1 className="text-2xl text-slate-900">
+      <span ref={el} />
+    </h1>
+  );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image"
+import HeroH1 from "./Hero-h1";
 
 export default function Hero() {
  return(
-    <div>Hero</div>
+    <HeroH1 />
  );
 }
